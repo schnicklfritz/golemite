4d56be2398d7f2b9b77f88659dee0cf452689437
Robert <Schnicklfritz@users.noreply.github.com>
2026-02-08 05:37:46 -0500

    updated start_chromium.sh to reflect change to openbox

--- scripts/start_chromium.sh
+++ scripts/start_chromium.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
-# 1. Enforce preferences (Disable "Ask where to save", set paths)
+k#!/bin/bash
+# 1. Enforce preferences
 python /home/golem/scripts/configure_chromium.py
 
 # 2. Build Flags
 FLAGS="--no-sandbox --disable-gpu --no-first-run --user-data-dir=/home/golem/.config/chromium"
 
 # 3. Kiosk vs Desktop Logic
-if [ "$DESKTOP_ENV" == "matchbox-window-manager" ]; then
-    # KIOSK: Full screen, no borders, no OS UI
+if [ "$DESKTOP_ENV" == "openbox" ]; then
+    # KIOSK MODE:
+    # --kiosk forces fullscreen
     FLAGS="$FLAGS --kiosk --start-maximized"
 else
-    # DESKTOP: Normal window
+    # DESKTOP MODE:
+    # Normal window with title bar
     FLAGS="$FLAGS --start-maximized"
 fi
 
-# 4. Launch opening the Landing Page
+# 4. Launch
 exec chromium $FLAGS "file:///home/golem/scripts/landing.html"
